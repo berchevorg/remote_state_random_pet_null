0d8991a7dd40cd739e4b9edc055b63888a585269
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2020-08-24 11:28:26 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,33 @@ data "terraform_remote_state" "platform" {
   }
 }
 
-resource "null_resource" "hello" {
-  provisioner "local-exec" {
-    command = "echo ${data.terraform_remote_state.platform.outputs.random_pet}"
+provider "aws" {
+  region = "us-east-1"  
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = data.terraform_remote_state.platform.outputs.random_pet
+  description = "Allow TLS inbound traffic"
+  
+    ingress {
+    from_port   = 445
+    to_port     = 445
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+  
+  
+
+# resource "null_resource" "hello" {
+#   provisioner "local-exec" {
+#     command = "echo ${data.terraform_remote_state.platform.outputs.random_pet}"
+#   }
+# }
